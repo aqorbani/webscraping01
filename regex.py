da509eba5f49781571a9bae4158cfd79599de216
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-09 21:33:57 +0330

    use regex methods in python

---
+++ regex.py
@@ -0,0 +1,14 @@
+import re
+
+# string = """amirhossein@gmail.com
+# javad330@yahoo.com
+# milad@outlook.com"""
+
+string = """web scraping in python"""
+
+result = re.split(r" ", string, 1)
+# result = re.sub(r"\w{6}$", "java", string)
+# result = re.findall(r"^\w+@(\w+)\.\w{2,3}", string, re.MULTILINE)
+# result = re.match(r"^\w{3}\s", string, re.MULTILINE)
+
+print(result)
