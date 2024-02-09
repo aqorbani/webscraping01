885956f50c95d2631a20ec8f9c5a1081ae1eae53
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-09 19:18:24 +0330

    use dumps method

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Web Scrapper...")
+import json
+
+data = {
+    "name": "Amir Hossein",
+    "age": "26",
+    "married": False,
+    "children": None
+}
+
+data_to_json = json.dumps(data)
+
+print(data_to_json)
