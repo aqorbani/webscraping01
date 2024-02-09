faa01fdd8e1b3e0612357d8194c748d7f1e9f820
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-09 20:13:23 +0330

    use loads method

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import json
 
-data = {
+data_json = """
+{
     "name": "Amir Hossein",
-    "age": "26",
-    "married": False,
-    "children": None
+    "age": 26,
+    "married": false,
+    "children": null
 }
+"""
 
 # data_to_json = json.dumps(data)
 #
 # print(data_to_json)
 
-with open("file.json", "w") as file:
-    json.dump(data, file, indent=4)
+# with open("file.json", "w") as file:
+#     json.dump(data, file, indent=4)
+
+data = json.loads(data_json)
+print(data)
