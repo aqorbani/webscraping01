c46fe502794edb737a6510cfb148d794e389b5da
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-13 22:18:48 +0330

    connect to openweathermap with api

--- request.py
+++ request.py
@@ -1,8 +1,9 @@
 import requests
 
-url = "https://toplearn.com/"
+api_key = "44c3e80e4895cf2a7c25cc15be402466"
+url = "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid="
 
-response = requests.get(url, timeout=2)
+response = requests.get(url + api_key, timeout=2)
 
 print(response.status_code)
 print(response.url)
