39d0baf373dac4dd94c3bd596f02e0d62f3d8d88
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-13 19:02:52 +0330

    testing some features

--- request.py
+++ request.py
@@ -2,6 +2,9 @@ import requests
 
 url = "https://toplearn.com/"
 
-response = requests.get(url)
+response = requests.get(url, timeout=2)
 
-print(response)
+print(response.status_code)
+print(response.url)
+print(response.text)
+print(response.cookies)
